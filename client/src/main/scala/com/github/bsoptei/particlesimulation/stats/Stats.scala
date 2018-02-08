f7c6d1015a93061b8c841ff237fa38d2e90cb645
bsoptei <bsoptei@gmail.com>
2018-02-08 21:17:15 +0100

    make stats organize stats string in a Map

--- client/src/main/scala/com/github/bsoptei/particlesimulation/stats/Stats.scala
+++ client/src/main/scala/com/github/bsoptei/particlesimulation/stats/Stats.scala
@@ -1,5 +1,7 @@
 package com.github.bsoptei.particlesimulation.stats
 
+import com.github.bsoptei.particlesimulation.shared.util.implicits._
+
 object Stats {
 
   def stats[T](xs: Iterable[T])(f: T => Double): String = {
@@ -7,6 +9,11 @@ object Stats {
     val wonderfulness = xs.map(f)
     val mean = wonderfulness.sum / size
 
-    s"Number of items: $size| Mean: $mean| Largest: ${wonderfulness.max} Smallest: ${wonderfulness.min}"
+    Map[String, String](
+      "numOfItems" -> size,
+      "mean"       -> mean,
+      "max"        -> s"${wonderfulness.max}",
+      "min"        -> s"${wonderfulness.min}"
+    ).toString
   }
 }
